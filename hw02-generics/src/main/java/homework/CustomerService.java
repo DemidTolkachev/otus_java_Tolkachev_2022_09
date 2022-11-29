737a19286434dc85423fef960b6c08124f0ee07c
DemidTolkachev <demid.tolkachev@mail.ru>
2022-11-29 11:29:31 +0300

    ДЗ 2

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,47 +1,27 @@
 package homework;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.AbstractMap;
+import java.util.*;
 
 public class CustomerService {
 
-    private Map<Customer, String> map = new HashMap<>();
+    private TreeMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(o -> o.getScores()));
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
-        Map.Entry<Customer, String> smallestScore = null;
+        Map.Entry<Customer, String> smallestScore = map.firstEntry();
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        for (Map.Entry<Customer, String> entry : map.entrySet()) {
-            if (smallestScore == null) {
-                smallestScore = new AbstractMap.SimpleEntry<Customer, String>(
-                        new Customer(entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()),
-                        entry.getValue());
-            } else {
-                if (entry.getKey().getScores() < smallestScore.getKey().getScores()) {
-                    smallestScore = new AbstractMap.SimpleEntry<Customer, String>(
-                            new Customer(entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()),
-                            entry.getValue());
-                }
-            }
-        }
-        return smallestScore;
+        return smallestScore == null ? null : new AbstractMap.SimpleEntry<Customer, String>(
+                new Customer(smallestScore.getKey().getId(), smallestScore.getKey().getName(),
+                        smallestScore.getKey().getScores()), smallestScore.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        Map.Entry<Customer, String> nextScore = null;
-
-        for (Map.Entry<Customer, String> entry : map.entrySet()) {
-            if ((entry.getKey().getScores() > customer.getScores()) &&
-                    (nextScore == null || (nextScore.getKey().getScores() > entry.getKey().getScores()))) {
-                nextScore = new AbstractMap.SimpleEntry<Customer, String>(
-                        new Customer(entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()),
-                        entry.getValue());
-            }
-        }
-        return nextScore;
+        Map.Entry<Customer, String> nextScore = map.higherEntry(customer);
+        return nextScore == null ? null : new AbstractMap.SimpleEntry<Customer, String>(
+                new Customer(nextScore.getKey().getId(), nextScore.getKey().getName(), nextScore.getKey().getScores()),
+                nextScore.getValue());
     }
 
     public void add(Customer customer, String data) {
